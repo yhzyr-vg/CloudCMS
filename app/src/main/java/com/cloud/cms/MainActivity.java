f15079fb56199205757d4ddcd54df4598ca3659e
chunlan.chen <chunlan.chen@baozun.com>
2019-07-11 18:43:34 +0800

    cms license 验证

--- app/src/main/java/com/cloud/cms/MainActivity.java
+++ app/src/main/java/com/cloud/cms/MainActivity.java
@@ -1,7 +1,9 @@
 package com.cloud.cms;
 
+import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.IntentFilter;
 import android.content.res.Configuration;
 import android.os.Bundle;
 import android.support.constraint.ConstraintLayout;
@@ -42,6 +44,8 @@ import com.cloud.cms.util.NetworkUtil;
 import com.cloud.cms.util.Validator;
 import com.cloud.cms.http.BaseOkHttpClient;
 import com.cloud.cms.widget.CustomDialog;
+import com.cloud.license.config.TVConfig;
+import com.cloud.license.manager.LicenseManager;
 import com.google.gson.Gson;
 
 import java.io.File;
@@ -55,45 +59,9 @@ import okhttp3.Call;
 
 public class MainActivity extends BaseActivity {
 
+    private static String tag="MainActivity";
     private Context context;
-    private static final String TAG="MainActivity";
-
-//    @BindView(R.id.tv_deviceName)
-//    TextView tv_deviceName;
-//    @BindView(R.id.progressBar)
-//    ProgressBar progressBar;
-//    @BindView(R.id.tv_prompt_isRegister)
-//    TextView tv_prompt_isRegister;
-//    @BindView(R.id.tv_prompt_noRegister)
-//    TextView tv_prompt_noRegister;
-//
-//    @BindView(R.id.main_layout)
-//    RelativeLayout main_layout;
-//
-//    @BindView(R.id.layout_progress)
-//    LinearLayout layout_progress;
-
-    private RelativeLayout main_layout;
-    private LinearLayout layout_progress, qr_layout;
-    private ImageView iv_QRcode;
-    private EditText et_code1;
-    private TextView tv_deviceName, tv_prompt_isRegister, tv_prompt_noRegister;
-    private ProgressBar progressBar;
-    private Button btn_commit;
-
-    /**
-     * 节目列表
-     */
-    private List<String> programList;
-
-    private String REGISTER_URL;
-
-    /**设备注册状态*/
-    private int registerState;
-
-    private CustomDialog dialog;
-
-    public static boolean isCheckedUpdate = false;
+    private BroadcastReceiver tvBroadcastReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -103,307 +71,48 @@ public class MainActivity extends BaseActivity {
         init();
     }
 
-    private void checkIsFirstStart(){
-        if (PreferenceManager.getInstance().getIsFirstStart()) {
-            PreferenceManager.getInstance().setIsFirstStart(false);
-            final File fileDir_cache = new File(Config.DIR_CACHE);
-            final File fileResource_info = new File(Config.RESOURCE_INFO);
-            if (fileDir_cache.exists() || fileResource_info.exists()) {
-                if (fileDir_cache.isFile() || fileResource_info.isFile()) {
-                    FileUtil.deleteFile(fileDir_cache);
-                    FileUtil.deleteFile(fileResource_info);
-                    init();
-                }
-            }
-        }else{
-            init();
-        }
-    }
-
-    /**
-     * 初始化
-     */
     private void init(){
-
-        main_layout=findViewById(R.id.main_layout);
-        layout_progress = (LinearLayout) findViewById(R.id.layout_progress);
-        tv_deviceName = (TextView) findViewById(R.id.tv_deviceName);
-        tv_deviceName.setText("deviceName:" + Config.DEVICE_NAME + "\ndeviceId:" + Config.SERIAL_NUMBER);
-        tv_prompt_isRegister = (TextView) findViewById(R.id.tv_prompt_isRegister);
-        tv_prompt_noRegister = (TextView) findViewById(R.id.tv_prompt_noRegister);
-        progressBar = (ProgressBar) findViewById(R.id.progressBar);
-
-        REGISTER_URL = "http://weixin.qq.com/r/2ygbAxjESkU1rVxg933J?macid=" + Config.SERIAL_NUMBER + "&memory_size="
-                + DeviceManager.getTotalInternalMemorySize() + "&memory_used="
-                + DeviceManager.getAvailableInternalMemorySize() + "&model=" + Config.DEVICE_MODEL;
-
-        programList = ProgramManager.getProgramListList();
-        if (Validator.isNullOrEmpty(programList)) {
-            progressBar.setVisibility(View.GONE);
-            tv_prompt_isRegister.setText(getResources().getString(R.string.tv_hasnotlocalresource));
-        } else {
-            progressBar.setVisibility(View.VISIBLE);
-            tv_prompt_isRegister.setText(getResources().getString(R.string.tv_startload));
-        }
-        if (isLand()) {
-            main_layout.setBackgroundResource(R.drawable.background_land);
-        } else {
-            main_layout.setBackgroundResource(R.drawable.background);
-        }
-        registerState = PreferenceManager.getInstance().getRegisterState();
-        if (registerState == DeviceConstants.REGISTER_STATE_YES) {//已注册
-            startTV();
-        }else{//未注册
-            if (NetworkUtil.isNetworkAvailable(context)){//有网络
-                checkRegisterState();//检查注册状态
-            }else{//无网络
-                showRegisterDialog();
-            }
-        }
+        // 广播监听license 校验的结果
+        initBroadcastReceiver();
+        //校验license
+        LicenseManager licenseManager=new LicenseManager(context);
+        licenseManager.setDelayTime(1*1000);//设置延迟时间。可自行调整 ，不设置默认15 秒
+       // licenseManager.setUserId(122);    //设置用户id  默认是131  请根据需要设置
+        licenseManager.checkLicense();
     }
 
-    private boolean isLand(){
-        if(getResources().getConfiguration().orientation == Configuration.ORIENTATION_LANDSCAPE){
-            return true;
-        }
-        return false;
-    }
-
-    private void login(LoginCommand loginCommand){
-        if(Validator.isNotNullOrEmpty(loginCommand)){
-            //step1 检查app 版本，比对本地版本号看是否需要更新
-            String version=PackageUtils.getAppVersionCode(context)+"";
-            Log.i(TAG,"========== current version:"+version);
-            //更新
-            if(Validator.isNotNullOrEmpty(loginCommand.getVersionCode())
-                    &&loginCommand.getVersionCode().compareTo(version)>0){
-                Log.i(TAG,"更新 current version:"+version+" 服务器版本："+loginCommand.getVersionCode());
-                String apkUrl=loginCommand.getApkurl();
-                Intent intent=new Intent(MainActivity.this,InstallAPKActivity.class);
-                intent.putExtra("apkurl",apkUrl);
-                intent.putExtra("description", loginCommand.getDescription());
-                intent.putExtra("type",InstallAPKActivity.TYPE_UPGRADE);
-               // startActivity(intent);
-               // return;
-            }
-
-            //step2 读取设备信息，设备是否已注册
-            if(Validator.isNotNullOrEmpty(loginCommand.getRows())){
-                Device device=loginCommand.getRows().get(0);
-                String registerState=device.getRegisterState();
-                if((DeviceConstants.REGISTER_STATE_YES+"").equals(registerState)){//已注册
-                    //节目列表不为空
-                    if(Validator.isNotNullOrEmpty(programList)){
-                        startActivity(new Intent(MainActivity.this,CMSActivity.class));
-                    }else{
-                        //startService(new Intent(MainActivity.this, MqttService.class));
-                        //getResource();
-                        ProgramManager.getResource();
-                    }
-                }else{//未注册，要求注册
-                    showRegisterDialog();
+    /**
+     * 广播
+     */
+    private void initBroadcastReceiver(){
+        IntentFilter filter=new IntentFilter(TVConfig.ACTION_CHECK_LICENSE_SUCCESS);
+        filter.addAction(TVConfig.ACTION_CHECK_LICENSE_FAILED);
+        tvBroadcastReceiver=new BroadcastReceiver() {
+            @Override
+            public void onReceive(Context context, Intent intent) {
+                String action=intent.getAction();
+                if(action.equals(TVConfig.ACTION_CHECK_LICENSE_SUCCESS)){ //校验 license 成功
+                    Log.i(tag,"==========ACTION_CHECK_LICENSE_SUCCESS");
+                    //成功启动cms
+                    Intent cmsntent = new Intent(context, CMSActivity.class);
+                    context.startActivity(cmsntent);
+                } else if(action.equals(TVConfig.ACTION_CHECK_LICENSE_FAILED)){  ////校验 license 失败
+                    Log.i(tag,"==========ACTION_CHECK_LICENSE_SUCCESS");
+                    //失败后要做的事,这里直接关闭窗口
+                    finish();
                 }
             }
-        }else{
-            showToast(context,"获取数据失败");
-        }
+        };
+        registerReceiver(tvBroadcastReceiver,filter);
     }
 
-
-
-
-
-
-    private void downloadResource(){
-        startActivity(new Intent(context,DownloadResource.class));
-    }
-
-    /**
-     * 获取资源
-     */
-    public  void getResource(){
-        BaseOkHttpClient.newBuilder().url(URLConstants.GET_SCHEDULE_URL)
-                .addParam("deviceid",Config.SERIAL_NUMBER)
-                .get()
-                .build()
-                .enqueue(new BaseCallBack(){
-
-                    @Override
-                    public void onSuccess(Object o) {
-                        if(Validator.isNotNullOrEmpty(o)){
-                            String str=o.toString();
-                            Log.i(TAG,"========"+o.toString());
-                            PreferenceManager.getInstance().setPrograms(str);
-                            downloadResource();
-                        }else{
-                            Log.e(TAG,"return: null");
-                        }
-                    }
-                    @Override
-                    public void onError(int code) {
-                        Log.e(TAG,"connect error"+code);
-                    }
-
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        Log.e(TAG,e.toString());
-                    }
-                });
-    }
-
-    /**
-     * 自动登录
-     */
-    private void autoLogin(){
-        BaseOkHttpClient.newBuilder().url(URLConstants.LOGIN_URL)
-                .addParam("deviceid",Config.SERIAL_NUMBER)
-                .addParam("memory_size",DeviceManager.getTotalInternalMemorySize())
-                .addParam("memory_used",DeviceManager.getAvailableInternalMemorySize())
-                .addParam("model",Config.DEVICE_MODEL)
-                .get()
-                .build()
-                .enqueue(new BaseCallBack(){
-
-                    @Override
-                    public void onSuccess(Object o) {
-                        showToast(context,o.toString());
-                        if(Validator.isNotNullOrEmpty(o)){
-                            String str=o.toString();
-                            LoginCommand loginCommand= JSON.parseObject(str,LoginCommand.class);
-                            login(loginCommand);
-                            Log.i(TAG,"=======connect:"+o.toString());
-                        }else{
-                            Log.e(TAG,"login: null");
-                            showRegisterDialog();
-                        }
-                    }
-
-                    @Override
-                    public void onError(int code) {
-                        showToast(context,"connect error:"+code);
-                        Log.e(TAG,"connect error"+code);
-                    }
-
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        showToast(context,"connect failure");
-                        Log.e(TAG,e.toString());
-                    }
-                });
-    }
-
-    /**
-     * 检查注册状态
-     */
-    private void checkRegisterState(){
-        BaseOkHttpClient.newBuilder().url(URLConstants.REGISTER_URL)
-                .addParam("sn",Config.SERIAL_NUMBER)
-                .addParam("memory_size",DeviceManager.getTotalInternalMemorySize())
-                .addParam("memory_used",DeviceManager.getAvailableInternalMemorySize())
-                .addParam("model",Config.DEVICE_MODEL)
-                .get()
-                .build()
-                .enqueue(new BaseCallBack(){
-
-                    @Override
-                    public void onSuccess(Object o) {
-                        Log.i(TAG,"register str:"+o);
-                        if(Validator.isNotNullOrEmpty(o)){
-                            String str=o.toString();
-                            RegisterCommand registerCommand= JSON.parseObject(str,RegisterCommand.class);
-                            checkRegisterState(registerCommand);
-                        }else{
-                            Log.e(TAG,"register: null");
-                            showRegisterDialog();
-                        }
-                    }
-
-                    @Override
-                    public void onError(int code) {
-                        showToast(context,"connect error:"+code);
-                        Log.e(TAG,"connect error"+code);
-                    }
-
-                    @Override
-                    public void onFailure(Call call, IOException e) {
-                        showToast(context,"connect failure");
-                        Log.e(TAG,e.toString());
-                    }
-                });
-    }
-
-    /**
-     * 注册
-     * @param registerCommand
-     */
-    private void checkRegisterState(RegisterCommand registerCommand){
-        if(Validator.isNotNullOrEmpty(registerCommand)){//返回数据
-            if(registerCommand.getCustomerId()!=0&&Validator.isNotNullOrEmpty(registerCommand.getCustomerName())){//已注册
-                registerState=DeviceConstants.REGISTER_STATE_YES;
-                PreferenceManager.getInstance().setRegisterState(registerState);
-                Config.USER_GROUP_NAME = registerCommand.getCustomerName();
-                PreferenceManager.getInstance().setUserName(registerCommand.getCustomerName());
-                showToast(MainActivity.this, getResources().getString(R.string.tip_registersuccess));
-                startTV();
-            }else{//未注册
-                showRegisterDialog();
-            }
-        }else{
-            showRegisterDialog();
-        }
-    }
-
-    /**
-     * 显示注册，显示二维码
-     */
-    private void showRegisterDialog(){
-        if (isLand()) {
-            main_layout.setBackgroundResource(R.drawable.background_land_qr);
-        } else {
-            main_layout.setBackgroundResource(R.drawable.background_qr);
-        }
-        showToast(context, getResources().getString(R.string.tip_registerfailbynotscan));
-        layout_progress.setVisibility(View.GONE);
-        Window window = dialog.getWindow();
-        WindowManager.LayoutParams lp = window.getAttributes();
-        if (isLand()) {
-            window.setGravity(Gravity.LEFT);
-            lp.x = Config.SCREEN_WIDTH / 5;
-        } else {
-            window.setGravity(Gravity.BOTTOM);
-            lp.y = Config.SCREEN_HEIGHT * 2 / 5;
-        }
-        lp.dimAmount = 0f;
-        window.setAttributes(lp);
-        dialog.show();
-    }
-
-    /**
-     *启动
-     */
-    private void startTV(){
-        if(Validator.isNotNullOrEmpty(Config.SERIAL_NUMBER)){
-            autoLogin();
-        }else{
-            showDailog(context,"提示","设备无法登陆！");
-        }
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
     protected void onDestroy() {
         super.onDestroy();
+        if(tvBroadcastReceiver!=null){
+            unregisterReceiver(tvBroadcastReceiver);
+            tvBroadcastReceiver=null;
+        }
     }
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-
-    }
 }
 
--- app/src/main/java/com/cloud/cms/activity/BootCompletedReceiver.java
+++ app/src/main/java/com/cloud/cms/activity/BootCompletedReceiver.java
@@ -5,6 +5,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import com.cloud.cms.MainActivity;
+
 public class BootCompletedReceiver  extends BroadcastReceiver {
     private static final String TAG=BootCompletedReceiver.class.getSimpleName();
 
@@ -12,7 +14,7 @@ public class BootCompletedReceiver  extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
             Log.d(TAG, "BOOT_COMPLETED,start CMSActivity……");
-            Intent cmsntent = new Intent(context, CMSActivity.class);
+            Intent cmsntent = new Intent(context, MainActivity.class);
             cmsntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
 			context.startActivity(cmsntent);
         }
